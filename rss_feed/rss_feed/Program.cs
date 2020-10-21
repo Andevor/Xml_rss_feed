1115a981f1f0dd0e4a63c45149aa5ce42ff65cee
Andevor <shadow362514@gmail.com>
2020-10-21 09:08:12 +0200

    XML usage by XDocument

--- rss_feed/rss_feed/Program.cs
+++ rss_feed/rss_feed/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace rss_feed
 {
@@ -6,7 +8,26 @@ namespace rss_feed
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            XDocument bbc = XDocument.Load("bbc.xml");
+            XDocument index = XDocument.Load("index.xml");
+            XDocument origo = XDocument.Load("origo.xml");
+            XDocument telex = XDocument.Load("telex.xml");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("BBC News:");
+            bbc.Root.Descendants("item").Select(x => x.Element("title").Value).ToList().ForEach(x => Console.WriteLine(x));
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Index News:");
+            index.Root.Descendants("item").Select(x => x.Element("title").Value).ToList().ForEach(x => Console.WriteLine(x));
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Origo News:");
+            origo.Root.Descendants("item").Select(x => x.Element("title").Value).ToList().ForEach(x => Console.WriteLine(x));
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Telex News:");
+            telex.Root.Descendants("item").Select(x => x.Element("title").Value).ToList().ForEach(x => Console.WriteLine(x));
+
+            Console.ForegroundColor = ConsoleColor.White;
+            //Console.WriteLine(bbc);
         }
     }
 }
